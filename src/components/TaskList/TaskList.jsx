6052808451d0dc9f8eccb6823253ab0cd725c81b
Vishal Srivastava <vsjee2002@gmail.com>
2025-12-15 00:27:24 +0530

    TaskList Cards 02

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div>
-        
+    <div id='tasklist' className='h-[40%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full bg-red-500 mt-10 py-5 rounded-lg'>
+        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 mx-5 rounded-xl'>
+
+        </div>
+
     </div>
   )
 }
