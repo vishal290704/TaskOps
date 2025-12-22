51af314fb0a664ec21829b68db5a6d5cd6270947
Vishal Srivastava <vsjee2002@gmail.com>
2025-12-22 11:16:42 +0530

    AllTask for admin task creation

--- src/other/AllTask.jsx
+++ src/other/AllTask.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthProvider'
 
 const AllTask = () => {
+
+  const authData = useContext(AuthContext)
+  console.log(authData.employees);
+  
   return (
     <div className='bg-[#1c1c1c] p-5 mt-10 rounded h-48 overflow-auto'>
-        <div className='bg-red-700 mb-2 py-2 px-4 flex justify-between rounded'>
+      {authData.employees.map(function(){
+        return  <div className='bg-red-700 mb-2 py-2 px-4 flex justify-between rounded'>
             <h2>Vishal</h2>
             <h3>Make a UI design</h3>
             <h5>Status</h5>
         </div>
-         <div className='bg-blue-700 mb-2 py-2 px-4 flex justify-between rounded'>
+      })}
+       
+         {/* <div className='bg-blue-700 mb-2 py-2 px-4 flex justify-between rounded'>
             <h2>Vishal</h2>
             <h3>Make a UI design</h3>
             <h5>Status</h5>
@@ -27,7 +35,7 @@ const AllTask = () => {
             <h2>Vishal</h2>
             <h3>Make a UI design</h3>
             <h5>Status</h5>
-        </div>
+        </div> */}
     </div>
   )
 }
