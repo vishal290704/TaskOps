2c0aad4ba7d27a30f1aa0bdd58415a2a2aea1198
Vishal Srivastava <vsjee2002@gmail.com>
2025-12-22 11:49:48 +0530

    Resolving issues

--- src/other/CreateTask.jsx
+++ src/other/CreateTask.jsx
@@ -7,25 +7,25 @@ const CreateTask = () => {
                 <div className='w-1/2'>
                  <div>
                  <h3 className='text-sm text-gray-300 mb-1'>Task title</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Make a UI design'/>
+                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4' type="text" placeholder='Make a UI design'/>
                </div>
                     <div>
                     <h3 className='text-sm text-gray-300 mb-1'>Date</h3>
-                <input className='[&::-webkit-calendar-picker-indicator]:invert text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="date" />
+                <input className='[&::-webkit-calendar-picker-indicator]:invert text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4' type="date" />
                 </div>
                 <div>
                     <h3 className='text-sm text-gray-300 mb-1'>Assign To</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Employee Name'/>
+                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4' type="text" placeholder='Employee Name'/>
                 </div>
                 <div>
                     <h3 className='text-sm text-gray-300 mb-1'>Category</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder="design, dev, etc"/>
+                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4' type="text" placeholder="design, dev, etc"/>
                 </div>
                 </div>
               
                 <div className='w-2/5 flex flex-col items-start'>
                     <h3 className='text-sm text-gray-300 mb-1'>Task Description</h3>
-                <textarea className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400' name='' id='' placeholder='Create a modern and responsive UI design for the new project.'></textarea>
+                <textarea className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border border-gray-400' name='' id='' placeholder='Create a modern and responsive UI design for the new project.'></textarea>
                 <button className='bg-emerald-600 bg-gray-2 hover:bg-emerald-700 px-5 py-2 rounded text-sm mt-4 w-full' type='submit'>Create Task</button>
                 </div>
                 
