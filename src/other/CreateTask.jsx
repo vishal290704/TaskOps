f7245ad085b2dcfa9383369b3a2d3b67dfafc126
Vishal Srivastava <vsjee2002@gmail.com>
2025-12-23 14:02:47 +0530

    Two way binding for all input and text area in admin CreateTask

--- src/other/CreateTask.jsx
+++ src/other/CreateTask.jsx
@@ -1,55 +1,96 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const CreateTask = () => {
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [taskDate, setTaskDate] = useState("");
+  const [assignTo, setAssignTo] = useState("");
+  const [category, setCategory] = useState("");
 
-    const [taskTitle, setTaskTitle] = useState('')
-    const [taskDescription, setTaskDescription] = useState('')
-    const [taskDate, setTaskDate] = useState('')
-    const [assignTo, setAssignTo] = useState('')
-    const [Category, setCategory] = useState('')
-    
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-        console.log("Task Created");
-        
-    }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Task Created");
+  };
   return (
-   <div className='p-5 bg-[#1c1c1c] mt-7 rounded'>
-            <form 
-            onSubmit={(e) => {
-                submitHandler(e);
-            }}
-            className='flex flex-wrap w-full items-start justify-between'>
-                <div className='w-1/2'>
-                 <div>
-                 <h3 className='text-sm text-gray-300 mb-1'>Task title</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4' type="text" placeholder='Make a UI design'/>
-               </div>
-                    <div>
-                    <h3 className='text-sm text-gray-300 mb-1'>Date</h3>
-                <input className='[&::-webkit-calendar-picker-indicator]:invert text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4' type="date" />
-                </div>
-                <div>
-                    <h3 className='text-sm text-gray-300 mb-1'>Assign To</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4' type="text" placeholder='Employee Name'/>
-                </div>
-                <div>
-                    <h3 className='text-sm text-gray-300 mb-1'>Category</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4' type="text" placeholder="design, dev, etc"/>
-                </div>
-                </div>
-              
-                <div className='w-2/5 flex flex-col items-start'>
-                    <h3 className='text-sm text-gray-300 mb-1'>Task Description</h3>
-                <textarea className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border border-gray-400' name='' id='' placeholder='Create a modern and responsive UI design for the new project.'></textarea>
-                <button className='bg-emerald-600 bg-gray-2 hover:bg-emerald-700 px-5 py-2 rounded text-sm mt-4 w-full' type='submit'>Create Task</button>
-                </div>
-                
-                
-            </form>
+    <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="flex flex-wrap w-full items-start justify-between"
+      >
+        <div className="w-1/2">
+          <div>
+            <h3 className="text-sm text-gray-300 mb-1">Task title</h3>
+            <input
+                value={taskTitle}
+                onChange={(e)=>{
+                    setTaskTitle(e.target.taskTitle)
+                }}
+              className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4"
+              type="text"
+              placeholder="Make a UI design"
+            />
+          </div>
+          <div>
+            <h3 className="text-sm text-gray-300 mb-1">Date</h3>
+            <input
+             value={taskDate}
+                onChange={(e)=>{
+                    setTaskDate(e.target.taskDate)
+                }}
+              className="[&::-webkit-calendar-picker-indicator]:invert text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4"
+              type="date"
+            />
+          </div>
+          <div>
+            <h3 className="text-sm text-gray-300 mb-1">Assign To</h3>
+            <input
+             value={assignTo}
+                onChange={(e)=>{
+                    setAssignTo(e.target.assignTo)
+                }}
+              className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4"
+              type="text"
+              placeholder="Employee Name"
+            />
+          </div>
+          <div>
+            <h3 className="text-sm text-gray-300 mb-1">Category</h3>
+            <input
+             value={category}
+                onChange={(e)=>{
+                    setCategory(e.target.category)
+                }}
+              className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4"
+              type="text"
+              placeholder="design, dev, etc"
+            />
+          </div>
         </div>
-  )
-}
 
-export default CreateTask
+        <div className="w-2/5 flex flex-col items-start">
+          <h3 className="text-sm text-gray-300 mb-1">Task Description</h3>
+          <textarea
+           value={taskDescription}
+                onChange={(e)=>{
+                    setTaskDescription(e.target.taskDescription)
+                }}
+            className="w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border border-gray-400"
+            name=""
+            id=""
+            placeholder="Create a modern and responsive UI design for the new project."
+          ></textarea>
+          <button
+            className="bg-emerald-600 bg-gray-2 hover:bg-emerald-700 px-5 py-2 rounded text-sm mt-4 w-full"
+            type="submit"
+          >
+            Create Task
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateTask;
