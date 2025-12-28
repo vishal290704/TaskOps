fb74c379e9ef43ea22af3a5ff3331b2cf760e47c
Vishal Srivastava <vsjee2002@gmail.com>
2025-12-28 14:14:33 +0530

    Wrapping ThemeContext

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import AuthProvider from './context/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  
+    <ThemeContext>
         <AuthProvider>
             <App />
         </AuthProvider>
+    </ThemeContext>
 
 )
