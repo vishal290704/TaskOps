1169026f55f4126af1c8f14d59897ca67d8462a5
Vishal Srivastava <vsjee2002@gmail.com>
2025-12-28 14:12:34 +0530

    created themeContext

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,12 +1,25 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const ThemeContext = createContext()
 
-const ThemeContext = () => {
-  return (
-    <div>
+const ThemeContext = ({children}) => {
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
 
-    </div>
+    useEffect(() => {
+      document.documentElement.classList.toggle("dark", theme === "dark")
+      localStorage.setItem("theme", theme)
+    
+     
+    }, [theme])
+    
+    const toggleTheme = ()=>{
+        setTheme((prev) =>(prev === "dark" ? "light" : "dark"))
+    }
+
+  return (
+   <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
   )
 }
 
